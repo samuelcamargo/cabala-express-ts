17006c1ce833829c91a9cbd8b15651cb8eaed812
samuel camargo <samuelcamargo1@gmail.com>
2024-11-12 08:51:03 -0300

    removido dependancia de banco da controler calcula cabala

--- src/controllers/CabalaControlle.ts
+++ src/controllers/CabalaControlle.ts
@@ -14,12 +14,22 @@ class CabalaController {
     }
   }
 
+
+  static async OrixaNameById(id: number): Promise<string | null> {
+    try {
+      const orixaRepository = getRepository(Orixa);
+      const orixa = await orixaRepository.findOne({ where: { id } });
+      return orixa ? orixa.name : null; // Retorna apenas o nome do Orixá
+    } catch (error) {
+      console.error("Erro ao buscar o nome do Orixá:", error);
+      return null;
+    }
+  }
+
   static async getOrixaNameById(req: Request, res: Response) {
     try {
       const id = parseInt(req.params.id, 10);
-      const orixaRepository = getRepository(Orixa);
-      const orixa = await orixaRepository.findOne({ where: { id } });
-
+      const orixa = await CabalaController.OrixaNameById(id);
       if (orixa) {
         res.status(200).json(orixa);
       } else {
@@ -38,10 +48,16 @@ class CabalaController {
 
       // Utiliza o CalculaCabalaController para calcular
       const cabalaCalculada = await CalculaCabalaController.calcula(ano, mes, dia);
-
       res.status(200).json({
         Nascimento: `Dia: ${dia} - Mês: ${mes} - Ano: ${ano}`,
-        Cabala: cabalaCalculada,
+        Cabala: {
+          "Dinheiro": await CabalaController.OrixaNameById(cabalaCalculada.dinheiro),
+          "Pessoas": await CabalaController.OrixaNameById(cabalaCalculada.pessoas),
+          "Coração": await CabalaController.OrixaNameById(cabalaCalculada.coracao),
+          "Racional": await CabalaController.OrixaNameById(cabalaCalculada.racional),
+          "Destino": await CabalaController.OrixaNameById(cabalaCalculada.destino),
+          "Fé": await CabalaController.OrixaNameById(cabalaCalculada.fe),
+        },
       });
     } catch (error) {
       res.status(400).json({
